6983c6784a5b1f4609f86c61d7ab14a440c57444
JoseMLM <jmlopezmuro@gmail.com>
2022-08-03 11:42:12 +0000

    Footer corregido

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
 return (
-  <div className="footer-expand-lg footer-white bg-black mt-5">
+  <div className="navbar navbar-dark navbar-expand-sm bg-dark d-flex justify-content-center text-white mt-5 mb-0"style={{height: "10vh"}}>
 <div className="text-center py-3 text-white"><span>© 2022 Copyright:</span>
   		  <a href="/">Jose Manuel Lopez Muro</a>
   		</div>
